7b49486a66cdffa5e7aa0f2fc3e2f6c6425df472
noelle529.github.io <noellecharter@gmail.com>
2022-06-23 17:16:03 -0400

    master

--- ExponentialGrowth.swift
+++ ExponentialGrowth.swift
@@ -1,15 +1,14 @@
 //Exponential Growth or Decay
 import Foundation
 var a:Float = 100.0 //intial amount
-var r:Float = 1.0 + 0.1 // total grow or decay 
-var x:Float // intial amount + rate
+var r:Float = 1.0 + 0.1 // rate calculation(growth) : to make DECAY change "addtion" symbol to "subtraction"
+var x:Float // total of intial amount + rate
 var t:Float = 8.0 //time
 var total:Float
 
+x = a * r // total of intial amount + rate
 
-x = a * r
-
-total = pow(x,t)
+total = pow(x,t)// this function returns the base raise to the power of exponent.
 
 
 print("\(total)")
