dfac84a62f117af669f870d859db0f848d645f2e
noelle529.github.io <noellecharter@gmail.com>
2022-07-24 14:28:04 -0400

    master

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SwiftTestin
-this is a collection Swift projects that I have completed and renewing 
-some projects have a part 2 because those projects came from an orginal project an I adapted it to test me skills and understanding
+This is a collection Swift projects that I have completed and renewing 
+some projects have a part 2 because those projects came from an orginal project and I adapted it to test my skills and understanding of swift development. 
---
+++ moresCodeProject.swift
@@ -0,0 +1,77 @@
+var englishText = "noelle is a secret message"
+var secretMessage = "-. --- . .-.. .-.. . / .. ... / .- / ... . -.-. .-. . - / -- . ... ... .- --. ."
+var letterToMorse : [String:String] = [
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--.."]
+//empty strings
+var morseText = ""
+var decodedMessage = ""
+var currMorse = ""
+//Array
+var morseCodeArray = [String]()
+var morseToLetter: [String:String] = [:]
+
+for element in englishText{
+    //print(element)
+    if let morseChar = letterToMorse["\(element)"]{
+        morseText += morseChar + " "
+    }else{
+        morseText += "   "
+    }
+    //print(morseText)
+}
+
+for char in secretMessage{
+    if char != " "{//this is checking to see if something has a space
+        currMorse.append(char)
+    }else{
+        switch currMorse {
+        case "":
+                currMorse += (" ")
+        case " ":
+            currMorse.append("")
+        default:
+                morseCodeArray.append(currMorse)
+                currMorse = ""
+        }
+    }
+}
+morseCodeArray.append(currMorse)
+//print(morseCodeArray)
+for (letter, morseChar) in letterToMorse {
+    morseToLetter[morseChar] = letter
+}
+//print(morseToLetter)
+for morseValue in morseCodeArray{
+    if let letterChar = morseToLetter[morseValue]{
+        decodedMessage += letterChar
+        //print(letterChar)
+    }else{
+        decodedMessage += " "
+    }
+}
+print(decodedMessage)
